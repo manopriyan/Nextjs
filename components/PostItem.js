6d319fc27c0f3c5ec045a4e16d4d28c10f61746d
manopriyan <manopriyan29@gmail.com>
2022-10-07 09:25:28 +0530

    initial

--- components/PostItem.js
+++ components/PostItem.js
@@ -18,7 +18,7 @@ function PostItem({post}) {
     }
 
   return (
-    <div className='col-md-7 p-3 m-3 boxshadow'>
+    <div className='col-md-7 p-3 m-3 boxshadow' key={post._id}>
         <h3>{post.title}</h3>
         <img src={post.imageUrl} className="img-fluid" height="400"></img>
 
